19c20db624051ec6fddf79395c3c3ebbc4da9b44
Fellipe Azambuja <femontanhanew@gmail.com>
2017-03-01 19:23:50 -0300

    Feature/packages setup (#1)
    * update README
    
    * setup packages.json
    
    * setup gulpfile
    
    * base folder scss

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var server = require('gulp-server-livereload');
+var bourbon = require('node-bourbon').includePaths;
+var normalize = require('node-normalize-scss').includePaths;
+
+gulp.task('webserver', ['watch'], function() {
+  gulp.src('./')
+    .pipe(server({
+      defaultFile: 'index.html',
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./assets/scss/**/*.scss')
+    .pipe(sass({
+      includePaths: [].concat(bourbon, normalize),
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css-min/'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./assets/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', [ 'sass', 'webserver' ]);
+gulp.task('watch', [ 'sass:watch' ]);
