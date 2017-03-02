22b463086d4639e06b5a6fadf69c66cc4cf64c98
Fellipe Azambuja <femontanhanew@gmail.com>
2017-03-02 14:20:19 -0300

    Setup form and events (#6)

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@ var Modal = (function() {
   var modal = document.querySelector(".c-modal");
   var closeModal = document.querySelector(".c-modal-close");
   var openModal = document.querySelector(".js-modal");
+  var form = document.querySelector('.c-form');
+  var feedback = document.querySelector('.c-modal-feedback');
 
   var open = function(e) {
     e.preventDefault();
@@ -10,13 +12,37 @@ var Modal = (function() {
   };
 
   var close = function(e) {
-    e.preventDefault();
+    if( !!e ) e.preventDefault();
+    reset();
     modal.className = 'c-modal';
-  }
+  };
+
+  var reset = function() {
+    form.reset();
+    feedback.className = 'c-modal-feedback';
+    feedback.innerHTML = '';
+  };
+
+  var submit = function(e) {
+    e.preventDefault();
+    var action = e.target.action;
+    var _self = this;
+
+    $.post(action, function (data) {
+      if( !!data && data.status === 'success') {
+        feedback.innerHTML = data.message;
+        feedback.className += ' c-modal-feedback--show';
+        setTimeout(close, 2000);
+      } else {
+        feedback.innerHTML = 'Aconteceu algum erro :/';
+      }
+    });
+  };
 
   var bindActions = function() {
     openModal.addEventListener('click', open, false);
     closeModal.addEventListener('click', close, false);
+    form.addEventListener('submit', submit, false);
   };
 
   var init = function() {
